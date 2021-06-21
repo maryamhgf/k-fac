c871473a9967bbe6e52600f7b2621fca4c06e7ca
Maryam Haghifam <haghifam.maryam@gmail.com>
2021-06-21 22:55:02 +0430

    Update toy.py

--- models/mnist/toy.py
+++ models/mnist/toy.py
@@ -11,12 +11,18 @@ class ToyNet(nn.Module):
         super(ToyNet, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
+            nn.LayerNorm([16, 28, 28], eps=1e-05, elementwise_affine=True, 
+                          device=None, dtype=None),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3),
             nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
+            nn.LayerNorm([16, 9, 9], eps=1e-05, elementwise_affine=True, 
+                          device=None, dtype=None),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3),
             nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
+            nn.LayerNorm([16, 3, 3], eps=1e-05, elementwise_affine=True, 
+                          device=None, dtype=None),
             nn.ReLU(),
             nn.Flatten(), 
             nn.Linear(3*3*16, 10)
