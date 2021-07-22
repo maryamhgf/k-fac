8fbeef0359f99bdd048cae616844efc39d8b3196
Maryam Haghifam <haghifam.maryam@gmail.com>
2021-07-22 09:13:18 +0430

    Fixed errors

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -62,14 +62,14 @@ def get_transforms(dataset):
 
     if dataset == 'imagenet':
         transform_train = transforms.Compose([
-            transforms.RandomCrop(224),
+            #transforms.RandomCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
         ])        
         transform_test = transforms.Compose([
-            transforms.RandomCrop(224),
+            #transforms.RandomCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
